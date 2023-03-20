25ad80b69b7df84b2641c4e5dc4da5be72fe1f3e
JDonMc <donkeycon@protonmail>
2023-03-20 18:56:17 +1100

    auto

--- mysite/aus_news_rss.py
+++ mysite/aus_news_rss.py
@@ -5,8 +5,8 @@ import time
 import feedparser
 from requests import get
 
-base = "http://export.arxiv.org/rss/"
-urls = ["astro-ph", "cond-mat", "cs", "econ", "eess", "gr-qc", "hep-ex", "hep-lat", "hep-ph", "hep-th", "math", "math-ph", "nlin", "nucl-ex", "nucl-th", "physics", "q-bio", "q-fin", "quant-ph", "stat"]
+base = ""
+urls = ["https://www.abc.net.au/news/feed/2942460/rss.xml", "http://www.9news.com.au/rss", "http://www.dailytelegraph.com.au/feed", "http://feeds.smh.com.au/rssheadlines/top.xml", "https://www.news.com.au/feed/", "http://feeds.theage.com.au/rssheadlines/top.xml", "https://www.heraldsun.com.au/rss", "https://www.perthnow.com.au/feed", "http://feeds.brisbanetimes.com.au/rssheadlines/top.xml", "https://www.canberratimes.com.au/rss.xml", "https://www.goldcoastbulletin.com.au/feed", "http://feeds.watoday.com.au/rssheadlines/top.xml", "https://www.townsvillebulletin.com.au/news/rss", "https://www.ntnews.com.au/news/rss", "https://westsidecommunitynews.com.au/feed/", "https://www.theguardian.com/au/rss", "https://www.theaustralian.com.au/feed/", "http://www.huffingtonpost.com.au/rss/index.xml", "https://bestinau.com.au/feed/", "https://tasmaniantimes.com/?/feeds/rss", "https://www.themercury.com.au/feed/", "https://www.northernstar.com.au/feeds/rss/homepage/", "https://www.coffscoastadvocate.com.au/feeds/rss/homepage/"]
 
 
 for rss_url in urls:
