766aa0d2f55efc13886ef96d4333f44d1c726021
JDonMc <donkeycon@protonmail>
2023-08-22 05:03:01 +1000

    auto

--- mysite/usa_news_rss.py
+++ mysite/usa_news_rss.py
@@ -23,7 +23,7 @@ for rss_url in urls:
         print(item.title)
         print(item.summary)
         
-        r = requests.post('https://www.predictionary.us/B/posts/',data={'title':item.title, "body":item.summary, "url": item.links[0].link})
+        r = requests.post('https://www.predictionary.us/B/posts/',data={'title':item.title, "body":item.summary, "url": item.link})
         print(r.status_code)
         print(r.text)
 
