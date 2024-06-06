c01a68610cc4f1521f83f39a97a7de3c5d60f06d
ADH AKA JDM <adenh@utas.edu.au>
2024-06-07 08:09:55 +1000

    auto

--- mysite/Bable/models.py
+++ mysite/Bable/models.py
@@ -847,8 +847,8 @@ DICTIONARY_SORT_CHOICES = (
 DICTIONARY_SORT_CHOICES_CHAR = (
 	("-latest_change_date", "Most Recent Change"),
 	("latest_change_date", "Least Recent Change"),
-	("-views", "Most Viewed"),
-	("views", "Least Viewed"),
+	("-viewcount", "Most Viewed"),
+	("viewcount", "Least Viewed"),
 	("-price", "Most Expensive"),
 	("price", "Least Expensive"),
 	("-creation_date", "Oldest"),
